dd2ad893b615785ae91df330e31756a5ea2222a9
Sivakrishnan C <c.skrishnan@hotmail.com>
2022-04-09 12:26:22 +0530

    First test case added

---
+++ PromoEngine/Engine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PromoEngine.Models;
+
+namespace PromoEngine;
+
+public class Engine
+{
+    public decimal CalculateInvoiceAmount(Order order)
+    {
+        return 0;
+    }
+}
--- PromoEngineTests/PromoEngineTests.cs
+++ PromoEngineTests/PromoEngineTests.cs
@@ -1,13 +1,24 @@
+using System.Collections.Generic;
 using Xunit;
+using PromoEngine;
+using PromoEngine.Models;
 
 namespace PromoEngineTests
 {
     public class PromoEngineTests
     {
-        [Fact]
-        public void Test1()
+        private Engine _engine;
+        public PromoEngineTests()
         {
+            _engine = new Engine();
+        }
 
+        [Fact]
+        public void OrderWithoutOrderDetailsReturnsZero()
+        {
+            Order order = new Order(1, new List<OrderDetail>());
+            decimal invoiceAmount = _engine.CalculateInvoiceAmount(order);
+            Assert.Equal(0, invoiceAmount);
         }
     }
 }
